4fab32e83f9d4b4b21729470eaf152badb04ab6b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-14 15:28:25 +0530

    move code to intro section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { IntroComponent } from './intro/intro.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, IntroComponent],
+  declarations: [AppComponent, HomeComponent, IntroComponent, ContactComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
