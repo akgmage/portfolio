7d80d22cb429518432ed89c835eb2924a074162c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-17 15:38:48 +0530

    add items array of object

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  items = [
+    {
+      title: 'asdas',
+      description: 'asdas da sd as das da sd as da ds as d asd as d',
+      technologies: ['HTML', 'CSS', 'Javascript'],
+    },
+    {
+      title: 'asdas',
+      description: 'asdas da sd as das da sd as da ds as d asd as d',
+      technologies: ['HTML', 'CSS', 'Javascript'],
+    },
+    {
+      title: 'asdas',
+      description: 'asdas da sd as das da sd as da ds as d asd as d',
+      technologies: ['HTML', 'CSS', 'Javascript'],
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
