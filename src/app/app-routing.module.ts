fb6ba7756bfa968b48cf4f5945cc78570b1e1ade
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-19 13:40:47 +0530

    configure route for resume

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: 'resume',
+    component: ResumeComponent,
   },
   {
     path: '**',
