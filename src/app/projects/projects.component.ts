439524312e0cdbe33767eaf3059e52c9cea08b94
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-19 14:02:33 +0530

    modify image path

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -13,7 +13,7 @@ export class ProjectsComponent implements OnInit {
       description:
         'Created a visualization tool for visualizing different pathfinding algorithms like (A*, Dijkstras, BFS, DFS) to find shortest paths between nodes in a graph.',
       technologies: ['HTML', 'CSS', 'Javascript'],
-      imageUrl: '../../assets/pathfindv.png',
+      imageUrl: 'assets/pathfindv.png',
     },
     {
       entityName: 'Company X',
@@ -21,7 +21,7 @@ export class ProjectsComponent implements OnInit {
       description:
         'Created a visualization tool for visualizing different sorting algorithms like Bubble sort, Insertion sort, Selection sort, Merge sort, Quick sort and Heap sort.',
       technologies: ['HTML', 'CSS', 'Javascript'],
-      imageUrl: '../../assets/sortingvis.png',
+      imageUrl: 'assets/sortingvis.png',
     },
     {
       entityName:
@@ -30,7 +30,7 @@ export class ProjectsComponent implements OnInit {
       description:
         'Ready recokner for structured screening programs for cervical and breast cancer. Quick and easy user experience for screening and reporting cases. Structuring a call-recall platform for clinical surveillance',
       technologies: ['Flutter', 'Dart', 'ASP.NET CORE'],
-      imageUrl: '../../assets/pathfindv.png',
+      imageUrl: 'assets/pathfindv.png',
     },
   ];
   constructor() {}
