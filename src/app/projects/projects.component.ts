91dd335ac4cd1670d16f60f31f16eb2748f29889
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-19 18:11:00 +0530

    replace project pictures, project information and resume

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,29 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent implements OnInit {
   items = [
     {
-      entityName: 'Company X',
-      title: 'Pathfinding Visualizer',
-      description:
-        'Created a visualization tool for visualizing different pathfinding algorithms like (A*, Dijkstras, BFS, DFS) to find shortest paths between nodes in a graph.',
-      technologies: ['HTML', 'CSS', 'Javascript'],
-      imageUrl: 'assets/pathfindv.png',
-    },
-    {
-      entityName: 'Company X',
-      title: 'Sorting Visualizer',
-      description:
-        'Created a visualization tool for visualizing different sorting algorithms like Bubble sort, Insertion sort, Selection sort, Merge sort, Quick sort and Heap sort.',
-      technologies: ['HTML', 'CSS', 'Javascript'],
-      imageUrl: 'assets/sortingvis.png',
-    },
-    {
       entityName:
         'A connected care platform to Simplify Healthcare Delivery for Women',
       title: 'GenVCare',
       description:
         'Ready recokner for structured screening programs for cervical and breast cancer. Quick and easy user experience for screening and reporting cases. Structuring a call-recall platform for clinical surveillance',
       technologies: ['Flutter', 'Dart', 'ASP.NET CORE'],
-      imageUrl: 'assets/pathfindv.png',
+      imageUrl: 'assets/threephone.png',
+    },
+    {
+      entityName:
+        'An educative tool to visualize different pathfinding algorithms',
+      title: 'Pathfinding Visualizer',
+      description:
+        'Created a visualization tool for visualizing different pathfinding algorithms like (A*, Dijkstras, BFS, DFS) to find shortest paths between nodes in a graph.',
+      technologies: ['HTML', 'CSS', 'Javascript'],
+      imageUrl: 'assets/pathvismac.png',
+    },
+    {
+      entityName: 'An educative tool to visualize different sorting algorithms',
+      title: 'Sorting Visualizer',
+      description:
+        'Created a visualization tool for visualizing different sorting algorithms like Bubble sort, Insertion sort, Selection sort, Merge sort, Quick sort and Heap sort.',
+      technologies: ['HTML', 'CSS', 'Javascript'],
+      imageUrl: 'assets/sortvismac.png',
     },
   ];
   constructor() {}
